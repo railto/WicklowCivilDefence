e8a0cfedc7872ae6516c1e33f03f74b08dd6d743
Mark Railton <mark@markrailton.com>
2021-01-12 10:28:27 +0000

    Installed and configured sanctum

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +32,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
-
+    'supports_credentials' => true,
 ];
