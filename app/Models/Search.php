aff66a543b3ebd8f257ecb6587436e213e23e87c
Mark Railton <mark@markrailton.com>
2021-01-20 20:41:20 +0000

    Adds soft delete to searches table

--- app/Models/Search.php
+++ app/Models/Search.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Search extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'created_by', 'location', 'start', 'type',
