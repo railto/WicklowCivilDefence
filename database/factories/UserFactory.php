840dce882c4e13df68725e0cc1880d4954ff6ab0
Mark Railton <mark@markrailton.com>
2021-01-20 19:15:24 +0000

    Add ability to make admin in factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name,
@@ -28,7 +28,20 @@ class UserFactory extends Factory
             'phone' => $this->faker->phoneNumber,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 'read',
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * @return UserFactory
+     */
+    public function admin(): UserFactory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'admin'
+            ];
+        });
     }
 }
